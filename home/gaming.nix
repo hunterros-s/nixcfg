c60dad457be36ef223e22bdbd4ba443ec4899d4b
hunterros-s <hlross@umich.edu>
2026-08-11 14:22:18 -0400

    some desktop stuff

--- home/gaming.nix
+++ home/gaming.nix
@@ -1,13 +1,6 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    steam
-    steam-run
-    gamescope
-    gamemode
-    mangohud
-    wineWow64Packages.stable
-    winetricks
     prismlauncher
   ];
 }
--- home/hyprland.nix
+++ home/hyprland.nix
@@ -4,7 +4,16 @@
     hyprland
     ghostty
     hyprlauncher
+    xwayland
   ];
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.adwaita-icon-theme;
+    name = "Adwaita";
+    size = 24;
+  };
 
   xdg.configFile."hypr/hyprland.lua".source = ./hyprland.lua;
 }
